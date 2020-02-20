5867fe31b29db8b06415bb58ce3df685e468d3ec
Tiger Cross <tc2017@ic.ac.uk>
2020-02-20 18:05:25 +0000

    Implement library class.

--- hashcode/solution.py
+++ hashcode/solution.py
@@ -1,1 +1,11 @@
 # A fine thing indeed!
+from typing import List
+
+class Library(object):
+
+    def __init__(self, books: List[int], signup_days: int, scan_limit: int):
+        self.books = books
+        self.signup_days = signup_days
+        self.scan_limit = scan_limit
+
+
