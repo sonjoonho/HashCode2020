7f7b5276c6f0140d534f3c34eae11ab35322e124
Tiger Cross <tc2017@ic.ac.uk>
2020-02-20 18:30:27 +0000

    Implement System.py

---
+++ hashcode/System.py
@@ -0,0 +1,8 @@
+class System(object):
+
+    def __init__(self, libraries):
+        self.libraries = libraries
+        self.sorted_libs = sorted(libraries, key=lambda l: l.signup_days)
+
+    # Should sort libraries by order of signup time increasing.
+    # Then perform second pass and scan based on book scores, removing duplicates (or adding them to a blacklist).
