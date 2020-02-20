51cc878fd714bebab15747b08b5c8a63e0b3e4f4
Joon-Ho Son <sonjoonh@gmail.com>
2020-02-20 18:22:43 +0000

    Fix bug in parsing code

--- hashcode/parse.py
+++ hashcode/parse.py
@@ -9,14 +9,19 @@ def parse_data(filename: str):
         n_libs = int(line_one[1])
         n_days = int(line_one[2])
         scores = [int(s) for s in data[1].split(' ')]
+
+        data = data[2:]
+
         libs = []
-        for i in range(0, n_libs, 2):
+        for i in range(0, (n_libs*2), 2):
             id = i//2
             lib_data = data[i].split(' ')
             n_lib_books = int(lib_data[0])
             signup_days = int(lib_data[1])
             scan_limit = int(lib_data[2])
             lib_books = [int(b) for b in data[i+1].split(' ')]
+
             libs.append(Library(id, n_lib_books, lib_books, signup_days, scan_limit))
+        assert n_libs == len(libs)
         return n_books, n_libs, n_days, scores, libs
 
--- hashcode/solution.py
+++ hashcode/solution.py
@@ -4,4 +4,7 @@ from typing import List
 from hashcode.parse import parse_data
 
 if __name__ == "__main__":
-    n_books, n_libs, n_days, scores, libs = parse_data("a_example.txt")
+    n_books, n_libs, n_days, scores, libs = parse_data("b_read_on.txt")
+    print(n_books)
+    print(n_libs)
+    print(libs)
