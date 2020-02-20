5bf3e500a7efd657bb2d1c97f1c2abe366ae7294
Will Burr <williamburr98@gmail.com>
2020-02-20 19:14:46 +0000

    Implement main loop

--- hashcode/system.py
+++ hashcode/system.py
@@ -1,16 +1,34 @@
 from typing import List
 
 from hashcode.library import Library
+from hashcode.signup import SignUp
 
 
 class System:
 
-    def __init__(self, libraries: List[Library], books: List[int]):
-        self.libraries = libraries
+    def __init__(self, libraries: List[Library], books: List[int], n_days: int):
+        self.libraries = sorted(self.libraries, key=lambda l: l.signup_days)
         self.books = books
+        self.n_days = n_days
 
-    def generate_solution(self):
-        sorted_libs = sorted(self.libraries, key=lambda l: l.signup_days)
+    def get_next_library(self, days_left: int, blacklist: List[int]):
+        pass
+
+    def generate_solution(self) -> List[SignUp]:
+        
+        days_left = self.n_days
+        current_lib = self.libraries[0]
+        result: [SignUp] = []
+        books_scanned: [int] = []
+        while current_lib is not None:
+            books = current_lib.get_books_scanned_from_initialization_day(days_left, self.books)
+            result.append(SignUp(current_lib.id, books))
+            for book in books:
+                books_scanned.append(book)
+            days_left -= current_lib.signup_days
+            current_lib = self.get_next_library(days_left, books_scanned)
+        return result
+            
 
     # Should sort libraries by order of signup time increasing.
     # Then perform second pass and scan based on book scores, removing duplicates (or adding them to a blacklist).
